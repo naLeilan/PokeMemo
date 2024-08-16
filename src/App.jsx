83bbd04771dfc03d63cd6640ae92ecfb87816f68
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-08-16 13:55:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function App() {
+  const [value, setValue] = useState("");
+  const [count, setCount] = useState(-1);
+
+  useEffect(() => setCount(count + 1), [value]);
+
+  const onChange = ({ target }) => setValue(target.value);
+
+  return (
+    <div>
+      <input type="text" value={value} onChange={onChange} />
+      <div>Number of changes: {count}</div>
+    </div>
+  );
+}
+
+export default App;
