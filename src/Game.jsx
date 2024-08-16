5579d4b6834918f58ca7d4a678e4d063a98f311a
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-08-16 14:23:23 +0300

    added ui

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,6 +2,21 @@ function Game() {
   return (
     <div className="app">
       <h3>Memory Card Game</h3>
+      <div className="rows">
+        <div className="row1">
+          <img src="1.jpg" alt="" />
+          <img src="2.jpg" alt="" />
+          <img src="3.jpg" alt="" />
+          <img src="4.jpg" alt="" />
+        </div>
+
+        <div className="row2">
+          <img src="5.jpg" alt="" />
+          <img src="6.jpg" alt="" />
+          <img src="7.jpg" alt="" />
+          <img src="2.jpg" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
