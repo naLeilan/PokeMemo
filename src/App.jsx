275b5308c68b78262e2e81a0d8a4e318bd57bdcc
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-08-16 14:09:34 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import Game from "./Game";
 
 function App() {
-  const [value, setValue] = useState("");
-  const [count, setCount] = useState(-1);
-
-  useEffect(() => setCount(count + 1), [value]);
-
-  const onChange = ({ target }) => setValue(target.value);
-
   return (
     <div>
-      <input type="text" value={value} onChange={onChange} />
-      <div>Number of changes: {count}</div>
+      <Game />
     </div>
   );
 }
---
+++ src/Game.jsx
@@ -0,0 +1,9 @@
+function Game() {
+  return (
+    <div className="app">
+      <h3>Memory Card Game</h3>
+    </div>
+  );
+}
+
+export default Game;
